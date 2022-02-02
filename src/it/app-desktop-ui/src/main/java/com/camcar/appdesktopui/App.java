0300a82633448ec63f81c6a3e5047e5b75e34b89
Cristian Camilo Cardozo Moreno <cristian.cardozo@globant.com>
2022-02-02 11:57:13 -0500

    First commit

---
+++ src/it/app-desktop-ui/src/main/java/com/camcar/appdesktopui/App.java
@@ -0,0 +1,13 @@
+package com.camcar.appdesktopui;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
